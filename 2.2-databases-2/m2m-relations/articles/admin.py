905e1861ca8a935d78b8e3cf53e01b9f02a6e544
RustamBH <kamaji@mail.ru>
2022-07-16 18:49:48 +0400

    Modified files in m2m-relations and orm_migrations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,40 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet, ValidationError
+from .models import Article, Tag, ArticleScope
 
-from .models import Article
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_category = 0
+        categories = []
+        for form in self.forms:
+            if form.cleaned_data.get('tag'):
+                if form.cleaned_data.get('tag').name in categories:
+                    raise ValidationError('Разделы дублируются')
+                else:
+                    categories.append(form.cleaned_data.get('tag').name)
+            if form.cleaned_data.get('is_main'):
+                main_category += 1
+
+        if main_category > 1:
+            raise ValidationError('Основным может только один раздел')
+        elif main_category == 0:
+            raise ValidationError('Выберите основной раздел')
+
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'text', 'published_at', 'image']
+    inlines = [ArticleScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-
-from articles.models import Article
+from articles.models import Article, ArticleScope, Tag
+from django.db.models.query import Prefetch
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = '-published_at'
+    articles = Article.objects.order_by(ordering).prefetch_related(
+        Prefetch('scopes', ArticleScope.objects.order_by('-is_main', 'tag__name')))
+    context = {'object_list': articles}
 
     return render(request, template, context)
